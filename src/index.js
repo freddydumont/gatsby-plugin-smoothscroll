7d66d8e04593676d10c69cab2dc8df34bbc0ef21
Frederick Morin <frederickmorin18@gmail.com>
2019-09-18 16:32:32 -0400

    Add comment to scrollTo function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * Smooth scrolling onClick event handler
+ * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
+ */
 const scrollTo = (selector) => {
   document.querySelector(selector).scrollIntoView({
     behavior: 'smooth',
