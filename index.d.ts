2148b8af24d46752a7b905ecbb5902c71884a34a
Frederick Morin <frederickmorin18@gmail.com>
2019-12-16 15:49:26 -0500

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Smooth scrolling onClick event handler
+ * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
+ */
+declare const scrollTo: (selector: string) => void;
+export default scrollTo;
