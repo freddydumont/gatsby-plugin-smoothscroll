f2cd6f7d012bb666143984bc5eab2fbbd75a3449
Frederick Morin <frederickmorin18@gmail.com>
2020-03-10 10:49:48 -0400

    Validate selector before calling `scrollIntoView`
    Fix #4

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 /**
  * Smooth scrolling onClick event handler
  * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
+ * @returns {boolean} false if `document.querySelector` doesn't find a match, otherwise true
  */
-declare const scrollTo: (selector: string) => void;
+declare const scrollTo: (selector: string) => boolean;
 export default scrollTo;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 /**
  * Smooth scrolling onClick event handler
  * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
+ * @returns {boolean} false if `document.querySelector` doesn't find a match, otherwise true
  */
 const scrollTo = (selector) => {
-  document.querySelector(selector).scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  });
+  const element = document.querySelector(selector);
+
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    return true;
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn(
+      "gatsby-plugin-smoothscroll:\n The selector: '%s' wasn't found in the document.\n Make sure you pass in a valid CSS selector string.",
+      selector
+    );
+  }
+
+  return false;
 };
 
 export default scrollTo;
