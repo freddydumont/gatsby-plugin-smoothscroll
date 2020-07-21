73227bf4362a799b8484201522a81ea97c8c53e7
Frederick Morin <frederickmorin18@gmail.com>
2020-07-21 11:41:45 -0400

    Update type information for new blockPosition param

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
 /**
  * Smooth scrolling onClick event handler
  * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
+ * @param {string} [blockPosition='start'] argument will be used to determine position where will be scrolled to
  * @returns {boolean} false if `document.querySelector` doesn't find a match, otherwise true
  */
-declare const scrollTo: (selector: string) => boolean;
+declare const scrollTo: (
+  selector: string,
+  blockPosition?: 'start' | 'center' | 'end' | 'nearest'
+) => boolean;
 export default scrollTo;
