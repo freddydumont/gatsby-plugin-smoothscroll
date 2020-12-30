3153c8891891fc09e4873d16254d54141b3a004f
Jonathan Hardisty <hardisty.jc@gmail.com>
2020-12-30 15:27:09 -0600

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,12 @@
  * Smooth scrolling onClick event handler
  * @param {string} selector argument will be passed to `querySelector`, usually an HTML id
  * @param {string} [blockPosition='start'] argument will be used to determine position where will be scrolled to
+ * @param {string} [offsetSelector=''] optional, usually a HTML id, used to determine an offset in the case of a fixed navbar
  * @returns {boolean} false if `document.querySelector` doesn't find a match, otherwise true
  */
 declare const scrollTo: (
   selector: string,
-  blockPosition?: 'start' | 'center' | 'end' | 'nearest'
+  blockPosition?: 'start' | 'center' | 'end' | 'nearest',
+  offsetSelector?: string,
 ) => boolean;
 export default scrollTo;
